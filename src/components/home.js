e4692f7691609acae2363c3c7448d9b573bd5c90
Raghvendra Awasthi <awasthiraghav59@gmail.com>
2021-06-09 15:25:06 +0530

    Auth added

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,6 @@ export default function Home(){
   return(
     <>
     <h1>Welcome, Raghav Awasthi</h1> 
-    <h4>{localStorage.getItem('CognitoIdentityServiceProvider.3i9iga2srecu9re5nqtkv0455b.LastAuthUser')}</h4>
     </>
   )
 }
