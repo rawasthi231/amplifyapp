2457cb5a450ca2bb35844414216a20109759283a
Raghvendra Awasthi <awasthiraghav59@gmail.com>
2021-06-09 15:21:33 +0530

    Auth added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { lazy, Suspense } from 'react';
+import './App.css';
+import "bootstrap";
 import { Route, Switch } from 'react-router-dom';
 import Navbar from "./components/navbar";
-import './App.css';
 import Loading from "./components/loading";
-//const Navbar = lazy(() => import("./components/navbar"));
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 const Home = lazy(() => import("./components/home"));
 const Users = lazy(() => import("./components/users"));
 const Chat = lazy(() => import("./components/chat"));
@@ -13,17 +14,35 @@ const About = lazy(() => import("./components/about"));
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={() => { return <Suspense fallback={<Loading />}><Home /></Suspense> }} />
+        <Route exact path='/about' component={() => { return <Suspense fallback={<Loading />}><About /></Suspense> }} />
+        <Route exact path='/users' component={() => { return <Suspense fallback={<Loading />}><Users /></Suspense> }} />
+        <Route exact path='/chat' component={() => { return <Suspense fallback={<Loading />}><Chat /></Suspense> }} />
+      </Switch>
+      <header>
+        <AmplifySignOut />
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
+
+/*
+
+
+
+function App() {
+  return (
+    <div className="App">
       <div className="container-fluid">
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={() => { return <Suspense fallback={<Loading />}><Home /></Suspense> }} />
-          <Route exact path='/about' component={() => { return <Suspense fallback={<Loading />}><About /></Suspense> }} />
-          <Route exact path='/users' component={() => { return <Suspense fallback={<Loading />}><Users /></Suspense> }} />
-          <Route exact path='/chat' component={() => { return <Suspense fallback={<Loading />}><Chat /></Suspense> }} />
-        </Switch>
+
       </div>
     </div>
   );
 }
 
 export default App;
+*/
--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Home(){
   return(
     <>
-    <h1>Home Page</h1> 
+    <h1>Welcome, Raghav Awasthi</h1> 
+    <h4>{localStorage.getItem('CognitoIdentityServiceProvider.3i9iga2srecu9re5nqtkv0455b.LastAuthUser')}</h4>
     </>
   )
 }
